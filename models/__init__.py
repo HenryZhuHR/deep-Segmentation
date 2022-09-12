06165f6bd9d214f9c7266f8b501d334514c27272
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2022-09-12 20:33:32 +0800

    train code

--- models/__init__.py
+++ models/__init__.py
@@ -20,28 +20,29 @@ from .modeling import (
 )
 from ._deeplab import convert_to_separable_conv
 
-MODEL_LIST:Dict[str,DeepLabV3]={
+MODEL_LIST: Dict[str, DeepLabV3] = {
     # Deeplab v3
-    'deeplabv3_hrnetv2_48':deeplabv3_hrnetv2_48,
-    'deeplabv3_hrnetv2_32':deeplabv3_hrnetv2_32,
-    'deeplabv3_resnet50':deeplabv3_resnet50,
-    'deeplabv3_resnet101':deeplabv3_resnet101,
-    'deeplabv3_mobilenet':deeplabv3_mobilenet,
-    'deeplabv3_xception':deeplabv3_xception,
+    'deeplabv3_hrnetv2_48': deeplabv3_hrnetv2_48,
+    'deeplabv3_hrnetv2_32': deeplabv3_hrnetv2_32,
+    'deeplabv3_resnet50': deeplabv3_resnet50,
+    'deeplabv3_resnet101': deeplabv3_resnet101,
+    'deeplabv3_mobilenet': deeplabv3_mobilenet,
+    'deeplabv3_xception': deeplabv3_xception,
 
     # Deeplab v3+
-    'deeplabv3plus_hrnetv2_48':deeplabv3plus_hrnetv2_48,
-    'deeplabv3plus_hrnetv2_32':deeplabv3plus_hrnetv2_32,
-    'deeplabv3plus_resnet50':deeplabv3plus_resnet50,
-    'deeplabv3plus_resnet101':deeplabv3plus_resnet101,
-    'deeplabv3plus_mobilenet':deeplabv3plus_mobilenet,
-    'deeplabv3plus_xception':deeplabv3plus_xception,
+    'deeplabv3plus_hrnetv2_48': deeplabv3plus_hrnetv2_48,
+    'deeplabv3plus_hrnetv2_32': deeplabv3plus_hrnetv2_32,
+    'deeplabv3plus_resnet50': deeplabv3plus_resnet50,
+    'deeplabv3plus_resnet101': deeplabv3plus_resnet101,
+    'deeplabv3plus_mobilenet': deeplabv3plus_mobilenet,
+    'deeplabv3plus_xception': deeplabv3plus_xception,
 }
 
-def get_model(model_name:str,num_classes=21, output_stride=8, pretrained_backbone=True):
+
+def get_model(model_name: str, num_classes=21, output_stride=8, pretrained_backbone=True) -> DeepLabV3:
     if model_name in MODEL_LIST:
-        model=MODEL_LIST[model_name]
-        return model(num_classes=num_classes, output_stride=output_stride, 
-        pretrained_backbone=pretrained_backbone)
+        model = MODEL_LIST[model_name]
+        return model(num_classes=num_classes, output_stride=output_stride,
+                     pretrained_backbone=pretrained_backbone)
     else:
-        raise KeyError('model_name only available in',MODEL_LIST)
+        raise KeyError('model_name only available in', MODEL_LIST)
--- train.py
+++ train.py
@@ -1,8 +1,15 @@
 
+import torch
+from torch import optim
 
+
+import utils
 from utils.args import ARGSer
-from models import get_model
 
+from models import get_model,convert_to_separable_conv
+from utils import set_bn_momentum
+
+from metrics import StreamSegMetrics
 
 def print_train_info(ARGS: ARGSer):
     print('Device', ARGS.DEVICE)
@@ -10,9 +17,35 @@ def print_train_info(ARGS: ARGSer):
 
 def main():
     ARGS = ARGSer()
-    model = get_model(ARGS.MODEL_ARCH, num_classes=ARGS.NUM_CLASSES,
+    model = get_model(ARGS.MODEL, num_classes=ARGS.NUM_CLASSES,
                       pretrained_backbone=True)
+    print(model)
+    if ARGS.SEPARABLE_CONV and ('plus' in ARGS.MODEL):
+        convert_to_separable_conv(model.classifier)
+    utils.set_bn_momentum(model.backbone, momentum=0.01)
 
+    # Set up metrics
+    metrics = StreamSegMetrics(ARGS.NUM_CLASSES)
+
+    # Set up optimizer
+    optimizer = optim.SGD(params=[
+        {'params': model.backbone.parameters(), 'lr': 0.1 * ARGS.LR},
+        {'params': model.classifier.parameters(), 'lr': ARGS.LR},
+    ], lr=ARGS.LR, momentum=0.9, weight_decay=ARGS.WEIGHT_DECAY)
+    # optimizer = torch.optim.SGD(params=model.parameters(), lr=opts.lr, momentum=0.9, weight_decay=opts.weight_decay)
+    # torch.optim.lr_scheduler.StepLR(optimizer, step_size=opts.lr_decay_step, gamma=opts.lr_decay_factor)
+
+    if ARGS.LR_SCHEDULE == 'poly':
+        scheduler = utils.PolyLR(optimizer, ARGS.total_itrs, power=0.9)
+    elif ARGS.LR_SCHEDULE == 'step':
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=ARGS.step_size, gamma=0.1)
+
+    # Set up criterion
+    # criterion = utils.get_loss(opts.loss_type)
+    if opts.loss == 'focal_loss':
+        criterion = utils.FocalLoss(ignore_index=255, size_average=True)
+    elif opts.loss_type == 'cross_entropy':
+        criterion = nn.CrossEntropyLoss(ignore_index=255, reduction='mean')
 
 if __name__ == '__main__':
     main()
