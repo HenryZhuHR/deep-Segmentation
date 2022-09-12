5ee1b623e4e75bcb1d0911655b86d52c7184c67e
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2022-09-12 23:24:04 +0800

    finish train

--- train.py
+++ train.py
@@ -1,15 +1,21 @@
 
+import os
+import time
+import tqdm
 import torch
+from torch import nn
+from torch import Tensor
 from torch import optim
-
-
+from torch.utils import data
 import utils
 from utils.args import ARGSer
 
-from models import get_model,convert_to_separable_conv
+from models import get_model, convert_to_separable_conv
 from utils import set_bn_momentum
-
+from datasets import ext_transforms as et
+from datasets .custom import SegmentationDataset
 from metrics import StreamSegMetrics
+
 
 def print_train_info(ARGS: ARGSer):
     print('Device', ARGS.DEVICE)
@@ -17,9 +23,20 @@ def print_train_info(ARGS: ARGSer):
 
 def main():
     ARGS = ARGSer()
+    LOG_FILE='%s/%s.txt'%(ARGS.SAVE_DIR,ARGS.SAVE_NAME)
+    open(LOG_FILE,'w').close()
+
+    train_set, valid_set = get_dataset(ARGS)
+    train_loader = data.DataLoader(train_set,  batch_size=ARGS.TRAIN_BATCH_SIZE,
+                                   shuffle=True, num_workers=ARGS.NUM_WORKERS)
+    valid_loader = data.DataLoader(valid_set,  batch_size=ARGS.VALID_BATCH_SIZE,
+                                   shuffle=False, num_workers=ARGS.NUM_WORKERS)
+    print("Dataset: %s, Train set: %d, Val set: %d" %
+          (ARGS.DATAROOT, len(train_set), len(valid_set)))
+
     model = get_model(ARGS.MODEL, num_classes=ARGS.NUM_CLASSES,
                       pretrained_backbone=True)
-    print(model)
+    model.to(ARGS.DEVICE)
     if ARGS.SEPARABLE_CONV and ('plus' in ARGS.MODEL):
         convert_to_separable_conv(model.classifier)
     utils.set_bn_momentum(model.backbone, momentum=0.01)
@@ -36,16 +53,106 @@ def main():
     # torch.optim.lr_scheduler.StepLR(optimizer, step_size=opts.lr_decay_step, gamma=opts.lr_decay_factor)
 
     if ARGS.LR_SCHEDULE == 'poly':
-        scheduler = utils.PolyLR(optimizer, ARGS.total_itrs, power=0.9)
+        scheduler = utils.PolyLR(optimizer, ARGS.EPOCHS, power=0.9)
     elif ARGS.LR_SCHEDULE == 'step':
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=ARGS.step_size, gamma=0.1)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, step_size=ARGS.STEP_SIZE, gamma=0.1)
 
     # Set up criterion
     # criterion = utils.get_loss(opts.loss_type)
-    if opts.loss == 'focal_loss':
+    if ARGS.LOSS_FUNCTION == 'focal_loss':
         criterion = utils.FocalLoss(ignore_index=255, size_average=True)
-    elif opts.loss_type == 'cross_entropy':
+    elif ARGS.LOSS_FUNCTION == 'cross_entropy':
         criterion = nn.CrossEntropyLoss(ignore_index=255, reduction='mean')
+    criterion.to(ARGS.DEVICE)
+
+    os.makedirs(ARGS.SAVE_DIR, exist_ok=True)
+
+    # ==========   Train Loop   ==========#
+    for epoch in range(ARGS.EPOCHS):
+        print('\033[32m', end='')
+        print('[Epoch]%d/%d' % (epoch, ARGS.EPOCHS), end=' ')
+        print('[Batch Size]%d/%d' %
+              (ARGS.TRAIN_BATCH_SIZE, ARGS.VALID_BATCH_SIZE), end=' ')
+        current_lr = optimizer.state_dict()['param_groups'][0]['lr']
+        print('[LR]%f' % (current_lr), end=' ')
+        print('[%s]' % (ARGS.DEVICE), end='  ')
+        print('\033[0m')
+
+        epoch_start_time = time.time()
+
+        model.train()
+        epoch_num = 0
+        epoch_loss = 0.
+        pbar = tqdm.tqdm(train_loader)
+        for images, labels in pbar:
+            images: Tensor = images.to(ARGS.DEVICE)
+            labels: Tensor = labels.to(ARGS.DEVICE)
+            labels=labels.long()
+            # torch.set_printoptions(profile="full")
+            # print(labels)
+            epoch_num += images.size(0)
+            optimizer.zero_grad()
+            outputs = model.forward(images)
+            loss = criterion.forward(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            batch_loss = loss.detach().cpu().numpy()
+            epoch_loss += batch_loss
+            epoch_info = 'Train Loss:%.4f' % batch_loss
+            pbar.set_description(epoch_info)
+        scheduler.step()
+        train_loss=epoch_loss/epoch_num
+
+        model.eval()
+        epoch_num = 0
+        epoch_loss = 0.
+        pbar = tqdm.tqdm(train_loader)
+        for images, labels in pbar:
+            images: Tensor = images.to(ARGS.DEVICE)
+            labels: Tensor = labels.to(ARGS.DEVICE)
+            labels=labels.long()
+            epoch_num += images.size(0)
+            with torch.no_grad():
+                outputs = model.forward(images)
+                loss = criterion.forward(outputs, labels)
+            batch_loss = loss.detach().cpu().numpy()
+            epoch_loss += batch_loss
+            epoch_info = 'Valid Loss:%.4f' % batch_loss
+            pbar.set_description(epoch_info)
+        valid_loss=epoch_loss/epoch_num
+
+        with open(LOG_FILE,'a') as f:
+            f.write('epoch:%d train_loss:%.6f valid_loss:%.6f'%(epoch,train_loss,valid_loss))
+
+        save_model ='%s/%s-epoch.pt'%(ARGS.SAVE_DIR, ARGS.SAVE_NAME)
+        torch.save(model.state_dict(),save_model)
+
+
+
+def get_dataset(ARGS: ARGSer):
+    train_transforms = et.ExtCompose([
+        et.ExtResize(size=ARGS.SIZE),
+        et.ExtRandomScale((0.5, 2.0)),
+        et.ExtRandomCrop(size=(ARGS.SIZE, ARGS.SIZE), pad_if_needed=True),
+        et.ExtRandomHorizontalFlip(),
+        et.ExtToTensor(),
+        et.ExtNormalize(mean=[0.485, 0.456, 0.406],
+                        std=[0.229, 0.224, 0.225]),
+    ])
+    val_transforms = et.ExtCompose([
+        et.ExtToTensor(),
+        et.ExtNormalize(mean=[0.485, 0.456, 0.406],
+                        std=[0.229, 0.224, 0.225]),
+    ])
+    train_set = SegmentationDataset(root=os.path.join(ARGS.DATAROOT, 'train'),
+                                    transforms=train_transforms)
+    valid_set = SegmentationDataset(root=os.path.join(ARGS.DATAROOT, 'valid'),
+                                    transforms=val_transforms)
+
+    return train_set, valid_set
+
 
 if __name__ == '__main__':
     main()
--- utils/args.py
+++ utils/args.py
@@ -14,58 +14,59 @@ class ARGSer:
         self.args = args
 
         # ====== Dataset ======
-        self.DATAROOT: str = args.data_root
-        self.DATASET: str = args.dataset
+        self.DATAROOT: str = os.path.expanduser(args.data_root)
         self.NUM_CLASSES: str = args.num_classes
+        self.SIZE: str = args.size
 
         # ====== Model ======
         self.MODEL: str = args.model
         self.SEPARABLE_CONV: str = args.separable_conv
         self.LOSS_FUNCTION: str = args.loss_function
-        
+
         # ====== Train ======
         self.DEVICE: str = torch.device(args.device)
-        self.MAX_EPOCH: int = args.max_epoch
+        self.TRAIN_BATCH_SIZE: int = args.train_batch_size
+        self.VALID_BATCH_SIZE: int = args.valid_batch_size
+        self.NUM_WORKERS: int = args.num_workers
+        self.EPOCHS: int = int(args.epochs)
         self.LR: float = args.lr
         self.LR_SCHEDULE: str = args.lr_schedule
         self.STEP_SIZE: int = args.step_size
-        self.WEIGHT_DECAY: float = args.weight_decay
-        
+        self.WEIGHT_DECAY: int = args.weight_decay
+
+        self.SAVE_DIR: str = args.save_dir
+        self.SAVE_NAME: str = args.save_name
 
     def init_args(self):
         parser = argparse.ArgumentParser()
         # ====== Dataset ======
-        parser.add_argument("--data_root", type=str, default='./datasets/data')
-        parser.add_argument("--dataset", type=str, default='voc',
-                            choices=['voc', 'cityscapes', 'custom'],)
-        parser.add_argument("--num_classes", type=int, default=21)
+        parser.add_argument('--data_root', type=str,
+                            default=os.path.expanduser('~/datasets/gc10_mask_mship'))
+        parser.add_argument('--num_classes', type=int, default=21)
+        parser.add_argument('--size', type=int, default=299)
 
         # ====== Model ======
-        parser.add_argument("--model", type=str, default='deeplabv3plus_resnet50')
-        parser.add_argument("--separable_conv", action='store_true', default=False,
-                        help="apply separable conv to decoder and aspp")
-        parser.add_argument("--loss_function", type=str, default='focal_loss')
-        
+        parser.add_argument('--model', type=str,
+                            default='deeplabv3plus_resnet50')
+        parser.add_argument('--separable_conv', action='store_true', default=False,
+                            help='apply separable conv to decoder and aspp')
+        parser.add_argument('--loss_function', type=str, default='focal_loss')
+
         # ====== Train ======
-        parser.add_argument("--device", type=str, default='cpu')
-        parser.add_argument("--max_epoch", type=int, default=30e3)
-        parser.add_argument("--lr", type=float, default=0.01)
-        parser.add_argument("--lr_schedule", type=str, default='poly', 
-        choices=['poly', 'step'])
-        parser.add_argument("--step_size", type=int, default=10000)
-        parser.add_argument("--weight_decay", type=float, default=1e-4)
+        parser.add_argument('--device', type=str, default='cpu')
+        parser.add_argument('--train_batch_size', type=int, default=4)
+        parser.add_argument('--valid_batch_size', type=int, default=4)
+        parser.add_argument('--num_workers', type=int, default=4)
+        parser.add_argument('--epochs', type=int, default=30e3)
+        parser.add_argument('--lr', type=float, default=0.01)
+        parser.add_argument('--lr_schedule', type=str, default='poly',
+                            choices=['poly', 'step'])
+        parser.add_argument('--step_size', type=int, default=10000)
+        parser.add_argument('--weight_decay', type=float, default=1e-4)
 
-        # PASCAL VOC
-        parser.add_argument("--year", type=str, default='2012',
-                            choices=['2012_aug', '2012', '2011', '2009', '2008', '2007'], help='year of VOC')
-
-        # ====== Deeplab Model ======
-        available_models = sorted(name for name in models.modeling.__dict__ if name.islower() and
-                                  not (name.startswith("__") or name.startswith('_')) and callable(
-            models.modeling.__dict__[name])
-        )
-        parser.add_argument("--model", type=str, default='deeplabv3plus_mobilenet',
-                            choices=available_models, help='model name')
+        parser.add_argument('--save_dir', type=str, default='checkpoints')
+        parser.add_argument('--save_name', type=str,
+                            default='deeplabv3plus_resnet50')
 
         args = parser.parse_args()
 
