f5883e33274bda695595a9ae1fa91fb442cc6f75
jin-yu-xin <2030888320@qq.com>
2022-09-12 20:30:41 +0800

    modify dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,15 +1,25 @@
 
 import os
+from typing import Tuple
+from torch import Tensor
 from torch.utils.data import Dataset
 
 class SegmentationDataset(Dataset):
-    def __init__(self,root,) -> None:
+    def __init__(self,root:str,) -> None:
         super().__init__()
         for dir in os.listdir(root):
             print(dir)
 
-def main():
-    train_set=SegmentationDataset(root='data/train')
+    def __getitem__(self, index:int)->Tuple[Tensor,Tensor]:
+        image:Tensor=None
+        label:Tensor=None
+        return image,label
+
+    def __len__(self)->int:
+        pass
 
 if __name__ == '__main__':
-    main()
+    train_set=SegmentationDataset(root='D:/projects/partsegvis/partsegvis/dataset/gc10_mask_mship/train')
+
+    print('image tensor',train_set[0][0])
+    print('label tensor',train_set[0][1])
